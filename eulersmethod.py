c8f8863b4db1ff418bb906ad74f8a308bdf98dc2
Nico31415 <nicoguitarrazurda@gmail.com>
2016-12-28 17:20:22 -0500

    Update eulersmethod.py

--- eulersmethod.py
+++ eulersmethod.py
@@ -1,11 +1,9 @@
 import numpy as np
 import math
 from matplotlib import pyplot as plt
-import parser
 
 x0 = input("Initial x: ")
 y0 = input("Initial y: ")
-Slope = input("Function for slope: ")
 xf = input("To what value of x do you want me to approximate y? ")
 h = input("h: ")
 n = int((xf-x0)/h)
@@ -13,7 +11,8 @@ x = np.linspace(x0,xf,n+1)
 y = np.zeros([n+1])
 y[0] = y0
 
-Slope = parser.expr(Slope).compile()
+def Slope(x,y):
+	return math.cos(x)
 
 for i in range(1,n):
 	y[i+1] = y[i] + Slope(x[i], y[i])*h
